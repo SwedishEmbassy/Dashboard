eb9e5fc1baa0283439099187fd7890c0f1ba005c
vnowen <viktoria@gmail.com>
2016-11-06 14:42:40 +0000

    Graphic fix and updated some weather codes

--- main.js
+++ main.js
@@ -38,6 +38,13 @@
               $('#weather').removeClass();
               $('#weather').addClass('cloudy');
                 break;
+            case '6':
+            case '8':
+            case '9':
+            case '10':
+            case '11':
+            case '12':
+            case '13':
             case '18':
             case '35':
             case '40':
@@ -62,14 +69,7 @@
               $('#weather').addClass('starry');
                 break;
             case '5':
-            case '6':
             case '7':
-            case '8':
-            case '9':
-            case '10':
-            case '11':
-            case '12':
-            case '13':
             case '14':
             case '15':
             case '16':
@@ -83,7 +83,7 @@
               $('#weather').removeClass();
               $('#weather').addClass('snowy');
                 break;
-          case '0':
+            case '0':
             case '1':
             case '2':
             case '3':
